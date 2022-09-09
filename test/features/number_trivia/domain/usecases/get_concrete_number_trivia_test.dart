0d028ab22efbc8b1e064e740ae993dec56a1e04c
imufiid <imufiid98@gmail.com>
2022-09-09 15:16:35 +0700

    fix: fixed import file

--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,4 +1,4 @@
-import 'package:dartz/dartz.dart';
+import 'package:dependencies/dartz/dartz.dart';
 import 'package:flutter_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:flutter_tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
